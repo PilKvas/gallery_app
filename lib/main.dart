6bf7da74cf30f7bf8e8ad0a830dca9c2e2b99def
darbinyan <darbinyan@webant.ru>
2023-11-21 12:21:14 +0300

    #2345 Добавил базовый экран для галлереи

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GalleryScreen extends StatefulWidget {
+class GalleryScreen extends StatelessWidget {
   const GalleryScreen({super.key});
 
   @override
-  State<GalleryScreen> createState() => _GalleryScreenState();
-}
-
-class _GalleryScreenState extends State<GalleryScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          TextField(),
-        ],
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            decoration: InputDecoration(
+              focusedBorder: const OutlineInputBorder(borderSide: BorderSide()),
+              contentPadding: const EdgeInsets.all(12),
+              hintText: 'Search',
+              suffixIcon: const Icon(
+                Icons.search,
+                color: Color.fromARGB(255, 237, 89, 146),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(),
+              ),
+            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(kToolbarHeight),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TabBar(
+                indicatorColor: Theme.of(context).tabBarTheme.indicatorColor,
+                tabs: const [
+                  Tab(
+                    text: 'New',
+                  ),
+                  Tab(
+                    text: 'Popular',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: const SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: TabBarView(
+                  children: <Widget>[
+                    Center(
+                      child: Text("New"),
+                    ),
+                    Center(
+                      child: Text("Popular"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
