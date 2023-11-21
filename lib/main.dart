71424bd99ee33263f36034a2ec993605d194d3a4
darbinyan <darbinyan@webant.ru>
2023-11-21 17:03:13 +0300

    #3323 | Добавил начальный слой презентации + добавил auto route в пабспек

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_generating_practice/presentation/extensions/theme/light_theme.dart';
+import 'package:flutter_generating_practice/presentation/screens/gallery_screen/gallery_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,77 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: lightTheme,
+      themeMode: ThemeMode.light,
       home: const GalleryScreen(),
     );
   }
 }
 
-class GalleryScreen extends StatelessWidget {
-  const GalleryScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: TextField(
-            decoration: InputDecoration(
-              focusedBorder: const OutlineInputBorder(borderSide: BorderSide()),
-              contentPadding: const EdgeInsets.all(12),
-              hintText: 'Search',
-              suffixIcon: const Icon(
-                Icons.search,
-                color: Color.fromARGB(255, 237, 89, 146),
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(),
-              ),
-            ),
-          ),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(kToolbarHeight),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: TabBar(
-                indicatorColor: Theme.of(context).tabBarTheme.indicatorColor,
-                tabs: const [
-                  Tab(
-                    text: 'New',
-                  ),
-                  Tab(
-                    text: 'Popular',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        body: const SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: TabBarView(
-                  children: <Widget>[
-                    Center(
-                      child: Text("New"),
-                    ),
-                    Center(
-                      child: Text("Popular"),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
